3ad4781aa65ec4616e90efbca1c01883a3a622dc
gyuhyeong <hyounghe0724@naver.com>
2022-12-02 14:41:18 +0900

    PORT env variable

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/User";
 import "./models/Comment"
 import app from "./server";
 
-const PORT = 57621;
+const PORT = process.env.PORT || 57621;
 
 const handleListening = () => {
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`); // 외부 접속을 받고 callback 함수  실행
