3bf3a822f36a562853dcb3f7f4fa7668d1b47e00
gyuhyeong <hyounghe0724@naver.com>
2022-12-03 13:28:27 +0900

    github created user avatar URl bug fixed

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -190,7 +190,7 @@ export const finishGithubLogin = async (req, res) => {
     let user = await User.findOne({ email: emailObj.email });
     if (!user) {
       user = await User.create({
-        avatarUrl: "/" + userData.avatar_url,
+        avatarUrl: userData.avatar_url,
         name: userData.name,
         username: userData.login,
         email: emailObj.email,
