d4c83df9ce1013d3cedd1dfe2bc82881f47f504b
gyuhyeong <hyounghe0724@naver.com>
2022-12-04 17:53:06 +0900

    aws apik key

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,19 @@
-const multer = require("multer");
+import multer from "multer"
+import multerS3 from "multer-s3"
+import aws from "aws-sdk"
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  }
+})
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: 'jjobtube',
+});
+
 export const localsMiddleWare = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.sideName = "Wetube";
@@ -29,11 +44,13 @@ export const uploadAvatar = multer({
   limits: {
     fileSize: 3000000,
   },
+  storage: multerUploader
 });
 export const uploadVideo = multer({
   dest: "uploads/videos/",
   limits: {
     fileSize: 10000000,
   },
+  storage: multerUploader
 });
 
